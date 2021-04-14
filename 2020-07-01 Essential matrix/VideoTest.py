6b1e8543b6bcd3daf1cd94ea1ac760c6c01ff44e
hedengran-skewl <m.hedengran47@gmail.com>
2021-04-14 15:39:08 +0200

    added pose estimation to videotest

--- 2020-07-01 Essential matrix/VideoTest.py
+++ 2020-07-01 Essential matrix/VideoTest.py
@@ -4,12 +4,73 @@ import numpy as np
 from codetiming import Timer
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+import symmetric_transfer_error
 
 #Img scaled to 40 percent for 
 Display_scale = 25
 Display_height = 2160 * Display_scale / 100
 Display_width= 3840 * Display_scale / 100
 Display_dim = (int(Display_width), int(Display_height))
+
+
+class PoseEstimation():
+    def EstimateRt(self, p1, p2, K):
+        E, mask = cv2.findEssentialMat(p1, p2, K, cv2.RANSAC, 0.999, 1.0)
+        M, H_mask = cv2.findHomography(p1, p2, cv2.RANSAC,5.0)
+        E_score = symmetric_transfer_error.checkEssentialScore(E, K, p1, p2)
+        H_score = symmetric_transfer_error.checkHomographyScore(M, p1, p2)
+
+        if E_score >= H_score:
+            points, R, t, mask = cv2.recoverPose(E, p1, p2)
+        
+        else:
+            H = np.transpose(H)
+            h1 = H[0]
+            h2 = H[1]
+            h3 = H[2]
+
+            Kinv = np.linalg.inv(K)
+
+            L = 1 / np.linalg.norm(np.dot(Kinv, h1))
+
+            r1 = L * np.dot(Kinv, h1)
+            r2 = L * np.dot(Kinv, h2)
+            r3 = np.cross(r1, r2)
+
+            t = L * np.dot(Kinv, h3)
+
+            R = np.array([[r1], [r2], [r3]])
+            R = np.reshape(R, (3, 3))
+            U, S, V = np.linalg.svd(R, full_matrices=True)
+
+            U = np.matrix(U)
+            V = np.matrix(V)
+            R = U * V
+            mask = H_mask
+        
+        matchesMask = mask.ravel().tolist()
+        return R, t, matchesMask
+    
+    def Triangulation(self, R, t, matches):
+        #calculate projection matrix for both camera
+        M_r = np.hstack((R, t))
+        M_l = np.hstack((np.eye(3, 3), np.zeros((3, 1))))
+
+        P_l = np.dot(K,  M_l)
+        P_r = np.dot(K,  M_r)
+
+        # undistort points
+        p1 = p1[np.asarray(matchesMask)==1,:,:]
+        p2 = p2[np.asarray(matchesMask)==1,:,:]
+        p1_un = cv2.undistortPoints(p1,K,None)
+        p2_un = cv2.undistortPoints(p2,K,None)
+        p1_un = np.squeeze(p1_un)
+        p2_un = np.squeeze(p2_un)
+
+        #triangulate points this requires points in normalized coordinate
+        point_4d_hom = cv2.triangulatePoints(P_l, P_r, p1_un.T, p2_un.T)
+        point_3d = point_4d_hom / np.tile(point_4d_hom[-1, :], (4, 1))
+        point_3d = point_3d[:3, :].T    
 
 class CameraSettings():
     def __init__(self):
@@ -22,13 +83,13 @@ class CameraSettings():
         self.distCoeffs = np.array(
                 [[ 0.0029, 0.0155, 0.014, 0.0022, 0.00025 ]])
     
-    def MatrixScaling():
+    def MatrixScaling(self):
         self.scale_factor = 0.20
         #self.scale_factor = 1
         self.cameraMatrix *= self.scale_factor
         self.cameraMatrix[2, 2] = 1
         
-        return cameraMatrix
+        return self.cameraMatrix, self.distCoeffs 
 
     def ImageScaling(self, img):
         width = int(img.shape[1] * self.scale_factor)
@@ -86,6 +147,8 @@ class FeatureExtractor(object):
         kp, des = self.descriptor.compute(img, kp)
         #Feature matching
         good = []
+        if len(kp) == 0:
+            return
         if self.last_frame is not None:
             #If less than 500 keypoints use bf matcher
             if len(kp) < 500:
@@ -120,7 +183,7 @@ class FeatureExtractor(object):
             cv2.putText(img, str(len(good)), (50,75), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 0.5, (255,0,0), 1, cv2.LINE_AA)
             cv2.putText(img, str(len(kp)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
         self.last_frame = {'kp': kp, 'des': des}
-        return img
+        return img, good
         
     @Timer(text="sift: {:.4f}")
     def sift_frame(self, img):
@@ -130,6 +193,8 @@ class FeatureExtractor(object):
         kp, des = self.sift.detectAndCompute(img, None)
         #Feature matching
         good = []
+        if len(kp) == 0:
+            return
         if self.last_frame_sift is not None:
             #If less than 500 keypoints use bf matcher
             if len(kp) < 500:
@@ -162,6 +227,8 @@ class FeatureExtractor(object):
         kp = self.orb.detect(img, None)
         kp, des = self.orb.compute(img, kp)
         #Feature matching
+        if len(kp) == 0:
+            return
         good = []
         if self.last_frame_orb is not None:
             #If less than 500 keypoints use bf matcher
@@ -196,6 +263,8 @@ class FeatureExtractor(object):
         kp, des = self.descriptor_orb.compute(img, kp)
         #Feature matching
         good = []
+        if len(kp) == 0:
+            return
         if self.last_frame_beblid is not None:
             #If less than 500 keypoints use bf matcher
             if len(kp) < 500:
@@ -227,7 +296,7 @@ if __name__=="__main__":
     while cap.isOpened():
         ret,frame = cap.read()
         if ret == True:
-            img = fe.process_frame(frame)
+            img, matches = fe.process_frame(frame)
             img_sift = fe.sift_frame(frame)
             img_orb = fe.orb_frame(frame)
             img_beblid = fe.beblid_frame(frame)
