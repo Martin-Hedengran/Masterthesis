04ccc127ba9aebe38af391bb2c7e62777e62e9f8
hedengran-skewl <marhe16@student.sdu.dk>
2021-04-27 15:46:48 +0200

    clean up frame.py

--- 2020-07-01 Essential matrix/Clean/frame.py
+++ 2020-07-01 Essential matrix/Clean/frame.py
@@ -27,19 +27,8 @@ def extractRt(E):
     ret[:3, :3] = R
     ret[:3, 3] = t
     return ret
-'''
-def extract(img):
-    orb = cv2.ORB_create()
-    # detection
-    pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
 
-    # extraction
-    kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in pts]
-    kps, des = orb.compute(img, kps)
 
-    # return pts and des
-    return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
-'''
 def extract(img):
     #Use Fast for feature detection, BEBLID for description.
     fast = cv2.FastFeatureDetector_create()
@@ -62,45 +51,7 @@ def denormalize(K, pt):
     ret /= ret[2]
     return int(round(ret[0])), int(round(ret[1]))
 
-'''
-def match_frames(f1, f2):
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches = bf.knnMatch(f1.des, f2.des, k=2)
 
-    # Lowe's ratio test
-    ret = []
-    idx1,idx2 = [], []
-
-    for m,n in matches:
-        if m.distance < 0.75*n.distance:
-            # keep around indices
-            idx1.append(m.queryIdx)
-            idx2.append(m.trainIdx)
-
-            p1 = f1.pts[m.queryIdx]
-            p2 = f2.pts[m.trainIdx]
-            ret.append((p1, p2))
-    assert len(ret) >= 8
-    ret = np.array(ret)
-    idx1 = np.array(idx1)
-    idx2 = np.array(idx2)
-
-    # fit matrix
-    model, inliers = ransac((ret[:, 0], ret[:, 1]),
-                            EssentialMatrixTransform,
-                            #FundamentalMatrixTransform,
-                            min_samples=8,
-                            #residual_threshold=1,
-                            residual_threshold=0.005,
-                            max_trials=200)
-    #print(sum(inliers), len(inliers))
-
-    # ignore outliers
-    Rt = extractRt(model.params)
-
-    # return
-    return idx1[inliers], idx2[inliers], Rt
-'''
 def EstimateRt(p1, p2, K):
     #Calculate lowest reprojection error between E and H, choose lowest
     E, mask = cv2.findEssentialMat(p1, p2, K, cv2.RANSAC, 0.999, 1.0)
